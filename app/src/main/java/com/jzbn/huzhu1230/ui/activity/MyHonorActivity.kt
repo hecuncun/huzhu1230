1d1a0a9801f4a9f959a7cd4af775a69132c8327f
hecuncun <13753638431@163.com>
2020-05-30 22:37:32 +0800

    UI细节修改

--- app/src/main/java/com/jzbn/huzhu1230/ui/activity/MyHonorActivity.kt
+++ app/src/main/java/com/jzbn/huzhu1230/ui/activity/MyHonorActivity.kt
@@ -1,9 +1,13 @@
 package com.jzbn.huzhu1230.ui.activity
 
 import BaseActivity
+import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.drawable.ColorDrawable
 import com.jzbn.huzhu1230.R
+import com.jzbn.huzhu1230.utils.StatusBarUtil
 import kotlinx.android.synthetic.main.activity_my_honor.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 /**
  * Created by hecuncun on 2020-5-25
@@ -15,7 +19,16 @@ class MyHonorActivity:BaseActivity() {
 
     }
 
+    override fun initStateBarColor() {
+        val mThemeColor =Color.parseColor("#FF431FC3")//设置状态栏颜色
+        StatusBarUtil.setColor(this, mThemeColor, 0)
+        if (this.supportActionBar != null) {
+            this.supportActionBar?.setBackgroundDrawable(ColorDrawable(mThemeColor))
+        }
+    }
+
     override fun initView() {
+        toolbar_title.text="个人荣誉"
         tv_honor_ruler.paint.flags = Paint.UNDERLINE_TEXT_FLAG; //下划线
         tv_honor_ruler.paint.isAntiAlias = true;//抗锯齿
         tv_honor_ji_li.paint.flags = Paint.UNDERLINE_TEXT_FLAG; //下划线
