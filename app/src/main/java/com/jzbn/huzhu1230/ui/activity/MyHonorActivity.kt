7532b1e3416c02aace83336bf8e5a8d788370f65
hecuncun <hecuncun>
2020-07-07 17:24:44 +0800

    保存到相册

--- app/src/main/java/com/jzbn/huzhu1230/ui/activity/MyHonorActivity.kt
+++ app/src/main/java/com/jzbn/huzhu1230/ui/activity/MyHonorActivity.kt
@@ -1,18 +1,27 @@
 package com.jzbn.huzhu1230.ui.activity
 
 import BaseActivity
+import android.content.Context
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.drawable.ColorDrawable
+import android.media.MediaScannerConnection
+import android.os.Build
+import android.os.Environment
 import com.jzbn.huzhu1230.R
 import com.jzbn.huzhu1230.bean.HonorInfoBean
+import com.jzbn.huzhu1230.ext.showToast
 import com.jzbn.huzhu1230.net.CallbackListObserver
 import com.jzbn.huzhu1230.net.SLMRetrofit
 import com.jzbn.huzhu1230.net.ThreadSwitchTransformer
+import com.jzbn.huzhu1230.utils.BitmapUtil
 import com.jzbn.huzhu1230.utils.StatusBarUtil
+import com.jzbn.huzhu1230.utils.ThreadUtils
+import com.jzbn.huzhu1230.utils.UriUtils
 import com.orhanobut.logger.Logger
 import kotlinx.android.synthetic.main.activity_my_honor.*
 import kotlinx.android.synthetic.main.toolbar.*
+import java.io.File
 
 /**
  * Created by hecuncun on 2020-5-25
@@ -57,6 +66,44 @@ class MyHonorActivity:BaseActivity() {
     }
 
     override fun initListener() {
+        tv_save.setOnClickListener {
+            //保存到本地
+             //val linearLayoutBitmap = BitmapUtil.getLinearLayoutBitmap(ll_container)
+             val linearLayoutBitmap = BitmapUtil.screenShot(this@MyHonorActivity)
+             val picPath= getDir(this)+ System.currentTimeMillis().toString() + "huzhu.jpg"
+             BitmapUtil.saveBitmap(linearLayoutBitmap, picPath)
+            if (Build.VERSION.SDK_INT >= 29) {
+                //适配android Q
+                ThreadUtils.runOnSubThread {
+                    UriUtils.saveImgToMediaStore(this@MyHonorActivity,picPath)
+                }
+            } else {
+                MediaScannerConnection.scanFile(
+                    this@MyHonorActivity.applicationContext,
+                    arrayOf(picPath),
+                    arrayOf("image/jpeg"),
+                    null
+                )
 
+            }
+            showToast("保存相册成功")
+
+        }
+    }
+
+    fun getDir(context: Context): String? {
+        val dir: String
+        dir = if (Build.VERSION.SDK_INT >= 29) {
+            context.getExternalFilesDir("").toString() + File.separator + "Media" + File.separator
+        } else {
+            (Environment.getExternalStorageDirectory()
+                .toString() + File.separator + "DCIM"
+                    + File.separator + "Camera" + File.separator)
+        }
+        val file = File(dir)
+        if (!file.exists()) {
+            file.mkdirs()
+        }
+        return dir
     }
 }
